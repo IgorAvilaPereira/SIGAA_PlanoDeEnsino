6986742397b06d7568126b4b0ec52075629163b7
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-08-14 08:52:01 -0300

    ok

--- background.js
+++ background.js
@@ -331,13 +331,13 @@ function script1(disciplina) {
   document.getElementById('form:adicionarAV').click();
   // }
 
-  // document.getElementById("form:tipo:3").checked = true;
+  document.getElementById("form:tipo:3").checked = true;
   document.getElementById("form:tipo:3").click();
+  document.getElementById("form:tipoIR:0").checked = true;
   document.getElementById("form:tipoIR:0").click();
   document.getElementById("form:descricao").value = "PPC - TADS";
   document.getElementById("form:url").value = "https://tads.riogrande.ifrs.edu.br/ppc";  
-  document.getElementById("form:adicionarIR").click();
-  // document.getElementById("form:tipoIR:0").checked = true;
+  document.getElementById("form:adicionarIR").click();  
   // document.getElementById("form:tipoIR").value = 1;
   
   // aulas
